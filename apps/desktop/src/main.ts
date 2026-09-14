246f8cbd909a281d8c80fed976131d4176ffcd40
Ankit soni <asoni.soni12@gmail.com>
2026-09-15 00:41:28 +0530

    add active window detection

--- apps/desktop/src/main.ts
+++ apps/desktop/src/main.ts
@@ -1,5 +1,12 @@
 import { app, BrowserWindow } from "electron";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { getActiveWindow } from "./active-window.js";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function craeteWindow(){
     const window = new BrowserWindow({
@@ -13,8 +20,23 @@ function craeteWindow(){
     window.loadURL("https://example.com");
 }
 
+async function monitorActiveWindow(){
+    setInterval(async() => {
+        try {
+            const activeWindow = await getActiveWindow()
+
+            console.log("Active Window:");
+            console.log(activeWindow);
+        } catch (error) {
+            console.error("Active window detection failed: ", error)
+        }
+    }, 2000);
+}
+
 app.whenReady().then(() => {
     craeteWindow();
+
+    monitorActiveWindow();
 
     app.on("activate", () => {
         if(BrowserWindow.getAllWindows().length === 0){
